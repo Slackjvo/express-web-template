48ccacb5b2825bc3b11f51a46ed198a222bd0136
Slackjvo <slackjvo@gmail.com>
2021-01-06 20:04:45 +0100

    some changes on mysql

--- models/example.js
+++ models/example.js
@@ -1,5 +1,5 @@
 //guide https://parzibyte.me/blog/2019/06/27/conexion-node-js-mysql-express/
-const mysqlConn = require("../db/mysqlConnection")
+const connection = require("../db/mysqlConnection")
 module.exports = {
 	
 }
